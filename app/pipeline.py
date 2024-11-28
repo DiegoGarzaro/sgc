0d360c7962841e0e01559cbec12d8112165e4cb2
DiegoGarzaro <diego.garzaro@gmail.com>
2024-11-28 01:52:57 -0300

    [CHORE] Set new users as inactive

--- app/pipeline.py
+++ app/pipeline.py
@@ -1,5 +1,25 @@
 from django.contrib import messages
 from django.shortcuts import redirect
+from django.contrib.auth import get_user_model
+from social_core.exceptions import AuthException
+
+
+User = get_user_model()
+
+def prevent_duplicate_user(backend, uid, user=None, *args, **kwargs):
+    """
+    Prevent duplicate accounts for the same email.
+    """
+    email = kwargs.get('details', {}).get('email')
+
+    # Check if the user already exists
+    if email and not user:
+        existing_user = User.objects.filter(email=email).first()
+        if existing_user:
+            raise AuthException(
+                backend,
+                "A user with this email already exists. Please contact support if this is unexpected."
+            )
 
 
 def remove_permissions(backend, user, *args, **kwargs):
@@ -16,10 +36,19 @@ def remove_permissions(backend, user, *args, **kwargs):
         user.save()
 
 
+def set_user_inactive(backend, user, *args, **kwargs):
+    """
+    Set the user as inactive after creation.
+    """
+    if user and user.is_active:  # Ensure this only applies to new users
+        user.is_active = False
+        user.save()
+
+
 def check_user_status(backend, user, request, *args, **kwargs):
     """
     Check if the user is active. If not, display a message and redirect to login.
     """
     if user and not user.is_active:
-        messages.error(request, 'Sua conta está pendente de aprovação.')
+        messages.error(request, 'Your account is pending approval by an administrator.')
         return redirect('login')
