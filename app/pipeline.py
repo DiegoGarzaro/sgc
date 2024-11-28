8845f74736687803269c1c3b913035daca410e63
DiegoGarzaro <diego.garzaro@gmail.com>
2024-11-28 02:11:42 -0300

    [CHORE] Google account adjustments - v2

--- app/pipeline.py
+++ app/pipeline.py
@@ -1,26 +1,5 @@
 from django.contrib import messages
 from django.shortcuts import redirect
-from django.contrib.auth import get_user_model
-from social_core.exceptions import AuthException
-
-
-User = get_user_model()
-
-def prevent_duplicate_user(backend, uid, user=None, *args, **kwargs):
-    """
-    Prevent duplicate accounts for the same email and associate existing users.
-    """
-    email = kwargs.get('details', {}).get('email')
-
-    # If no user is associated and email exists, try to find an existing user
-    if email and not user:
-        existing_user = User.objects.filter(email=email).first()
-        if existing_user:
-            # Associate the existing user with the social account
-            return {'user': existing_user}
-
-    # If no email or no user found, allow the pipeline to create a new user
-    return {}
 
 
 def remove_permissions(backend, user, *args, **kwargs):
@@ -37,19 +16,10 @@ def remove_permissions(backend, user, *args, **kwargs):
         user.save()
 
 
-def set_user_inactive(backend, user, *args, **kwargs):
-    """
-    Set newly created users as inactive.
-    """
-    if user and not user.is_active:  # Apply only to new users
-        user.is_active = False
-        user.save()
-
-
 def check_user_status(backend, user, request, *args, **kwargs):
     """
     Check if the user is active. If not, display a message and redirect to login.
     """
     if user and not user.is_active:
-        messages.error(request, 'Your account is pending approval by an administrator.')
+        messages.error(request, 'Sua conta está pendente de aprovação.')
         return redirect('login')
--- app/settings.py
+++ app/settings.py
@@ -169,12 +169,10 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_details',
     'social_core.pipeline.social_auth.social_uid',
     'social_core.pipeline.social_auth.auth_allowed',
-    'social_core.pipeline.social_auth.social_user',  # Look for an existing user
-    'app.pipeline.prevent_duplicate_user',          # Prevent duplicate accounts
+    'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
-    'social_core.pipeline.user.create_user',        # Create the user if not existing
-    'app.pipeline.set_user_inactive',               # Set new user as inactive
-    'app.pipeline.check_user_status',               # Check user status and redirect if needed
+    'social_core.pipeline.user.create_user',  # Cria o usuário se não existir
+    'app.pipeline.check_user_status',    # Função personalizada
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
