3938753437eb17a9fced4cf179b2976cee4ca171
DiegoGarzaro <diego.garzaro@gmail.com>
2025-06-19 20:14:43 -0300

    [FIX] Apply static analysis corrections

--- components/querystring_tags.py
+++ components/querystring_tags.py
@@ -2,15 +2,16 @@ from django import template
 
 register = template.Library()
 
+
 @register.simple_tag(takes_context=True)
 def pagination_query(context):
     """Return existing query parameters excluding the page param."""
-    request = context.get('request')
+    request = context.get("request")
     if not request:
-        return ''
+        return ""
     querydict = request.GET.copy()
-    querydict.pop('page', None)
+    querydict.pop("page", None)
     query = querydict.urlencode()
     if query:
-        return '&' + query
-    return ''
+        return "&" + query
+    return ""
